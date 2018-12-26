a4caecc32d83c07d4e0ceecf7f2b3a5bf1ef4568
Pavlo_Padalka <Pavlo_Padalka@epam.com>
2018-12-26 16:40:37 +0200

    test commit

--- test-service/src/main/java/springcloudwithkeycloak/testservice/controller/TestController.java
+++ test-service/src/main/java/springcloudwithkeycloak/testservice/controller/TestController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/tst")
-
     public String test() {
         log.info("Incoming request");
         return "test";
